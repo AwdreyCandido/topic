85b2a68067f63e067593b3963b7b8e1c8df8a213
Awdrey <awdrey@hippo-tech.com>
2025-03-05 19:17:26 -0300

    expanding card style

--- src/components/topic-card/TopicCard.tsx
+++ src/components/topic-card/TopicCard.tsx
@@ -1,37 +1,88 @@
-import { HiEllipsisVertical, HiMiniFolder, HiMiniPlus } from "react-icons/hi2";
+import {
+  HiEllipsisVertical,
+  HiMiniFolder,
+  HiMiniPlus,
+  HiMiniXMark,
+} from "react-icons/hi2";
 import styles from "./TopicCard.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const TopicCard = () => {
   const [expand, setExpand] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
 
   const handleExpand = () => {
+    const rect = cardRef.current.getBoundingClientRect();
+    // console.log(rect.top, rect.left, rect.width, rect.height);
+    cardRef.current?.style.setProperty("--initial-top", `${rect.top}px`);
+    cardRef.current?.style.setProperty("--initial-left", `${rect.left}px`);
+    cardRef.current?.style.setProperty("--initial-width", `${rect.width}px`);
+    cardRef.current?.style.setProperty("--initial-height", `${rect.height}px`);
+
     setExpand((prev) => !prev);
   };
 
   return (
-    <div onClick={handleExpand} className={`${styles.card} ${expand ?styles.expand : ''} pop-in`}>
-      <div>
-        <div className={styles.title}>
-          <p>Topic Card</p>
-          <div className={styles.options}>
-            <HiEllipsisVertical />
+    <div
+      ref={cardRef}
+      className={`${styles.card} pop-in ${expand ? "expand" : "pop-in"}`}
+      // className={`${expand ? styles.expand : styles.card} ${
+      //   expand ? "expand" : "pop-in"
+      // }`}
+    >
+      {!expand ? (
+        <div onClick={handleExpand}>
+          <div>
+            <div className={styles.title}>
+              <p>Topic Card</p>
+              <div className={styles.options}>
+                <HiEllipsisVertical />
+              </div>
+            </div>
+            <div className={styles.iconContainer}>
+              <div className={styles.icon}>
+                <HiMiniFolder />
+              </div>
+              <p>Empty</p>
+            </div>
+          </div>
+          <div className={styles.actionContainer}>
+            <div className={styles.dashedIcon}>
+              <HiMiniPlus />
+            </div>
+            <p>Last updated at: 01/04/2025</p>
           </div>
         </div>
-        <div className={styles.iconContainer}>
-          <div className={styles.icon}>
-            <HiMiniFolder />
-          </div>
-          <p>Empty</p>
-        </div>
-      </div>
+      ) : (
+        <div className="duration-300">
+          <div className={styles.navbar}>
+            <div className="flex gap-10">
+              <div className={styles.info}>
+                <h2>Topic Card</h2>
+                <p>Last updated at: 01/04/2025</p>
+              </div>
 
-      <div className={styles.actionContainer}>
-        <div className={styles.addIcon}>
-          <HiMiniPlus />
+              <div className={styles.iconContainer}>
+                <div className={styles.icon}>
+                  <HiMiniFolder />
+                </div>
+                <p>Empty</p>
+              </div>
+            </div>
+            <div className={styles.navActions}>
+              <div onClick={handleExpand} className={styles.dashedIcon}>
+                <HiMiniXMark />
+              </div>
+              <div className={styles.dashedIcon}>
+                <HiMiniPlus />
+              </div>
+              <div className={styles.dashedIcon}>
+                <HiEllipsisVertical />
+              </div>
+            </div>
+          </div>
         </div>
-        <p>Last updated at: 01/04/2025</p>
-      </div>
+      )}
     </div>
   );
 };
